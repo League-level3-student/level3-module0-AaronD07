c2475ae6d64fe118b7a5997b59dce0b0009a61a9
AaronD07 <38016960+AaronD07@users.noreply.github.com>
2020-04-28 15:26:08 -0700

    :smile:

--- src/_00_IntroToArrays/_00_ArrayCheatSheet.java
+++ src/_00_IntroToArrays/_00_ArrayCheatSheet.java
@@ -1,7 +1,7 @@
 package _00_IntroToArrays;
 
-import java.util.ArrayList;
-import java.util.Collections;
+
+
 import java.util.Random;
 
 import javax.swing.JOptionPane;
@@ -9,46 +9,59 @@ import javax.swing.JOptionPane;
 public class _00_ArrayCheatSheet {
 	public static void main(String[] args) {
 		// 1. make an array of 5 Strings
-		ArrayList<String> list = new ArrayList<String>();
-		list.add(new String("2"));
-		list.add(new String("1"));
-		list.add(new String("0"));
-		list.add(new String("3"));
-		list.add(new String("4"));
-
+		String[] list = { "0", "5", "4", "1", "2"};
 		// 2. print the third element in the array
-		JOptionPane.showMessageDialog(null, list.get(2));
+		JOptionPane.showMessageDialog(null, list[2]);
 		// 3. set the third element to a different value
-		list.set(2, "10");
+		list[2]="10";
 		// 4. print the third element again
-		JOptionPane.showMessageDialog(null, list.get(2));
+		JOptionPane.showMessageDialog(null, list[2]);
 		// 5. use a for loop to set all the elements in the array to a string of your
 		// choice
-for(int i =0; i<list.size(); i++) {
-	JOptionPane.showMessageDialog(null, list);
+for(int i =0; i<list.length; i++) {
+	JOptionPane.showMessageDialog(null, list[i]);
 	// 6. use a for loop to print all the values in the array
 }
 		// BE SURE TO USE THE ARRAY'S length VARIABLE
-ArrayList<Integer> numbers = new ArrayList<Integer>(50);
-
-
+Integer[] numbers = new Integer[50];
 		// 7. make an array of 50 integers
-
 		// 8. use a for loop to make every value of the integer array a random number
-for(int o=0; o<numbers.size(); o++) {
+for(int o=0; o<numbers.length; o++) {
 	Random random = new Random();
-	numbers.set(o, random.nextInt());
+numbers[o]=random.nextInt(100);
+System.out.println(numbers[o]);
 }
 
 		// 9. without printing the entire array, print only the smallest number on the
 		// array
 
+int x=numbers[0];
+for(int i=0; i<numbers.length; i++) {
+if(numbers[i]<x) {
+x=numbers[i];
 
+}
+}
+System.out.println(x);
+
+
+int y=numbers[0];
+for(int i=0; i<numbers.length; i++) {
+if(numbers[i]>y) {
+y=numbers[i];
+
+}
+}
+System.out.println(y);
+
+
+System.out.println(numbers[49]);
+}
 		// 10 print the entire array to see if step 8 was correct
 
 		// 11. print the largest number in the array.
 
 		// 12. print only the last element in the array
-System.out.println(numbers.lastIndexOf(numbers));
-	}
+
+	
 }
--- src/_00_IntroToArrays/_01_RobotRace.java
+++ src/_00_IntroToArrays/_01_RobotRace.java
@@ -1,5 +1,6 @@
 package _00_IntroToArrays;
 
+import java.util.ArrayList;
 import java.util.Random;
 
 import javax.swing.JOptionPane;
@@ -7,24 +8,40 @@ import javax.swing.JOptionPane;
 import org.jointheleague.graphical.robot.Robot;
 
 public class _01_RobotRace {
-	//1. make a main method
-	
-		//2. create an array of 5 robots.
+	// 1. make a main method
+	public static void main(String[] args) {
+		Robot[] robs = new Robot[5];
+		for(int i=0; i<robs.length; i++) {
+			robs[i]=new Robot();
+			robs[i].setX(i*200);
+			robs[i].setY(600);
+			Random random = new Random(50);
+			robs[i].move(random.nextInt()-60);
+		}
+		
+		
+		
+		
+		
+		}
+		
+	}
+	// 2. create an array of 5 robots.
 
-		//3. use a for loop to initialize the robots.
+	// 3. use a for loop to initialize the robots.
 
-			//4. make each robot start at the bottom of the screen, side by side, facing up
-	
-		//5. use another for loop to iterate through the array and make each robot move 
-	    //   a random amount less than 50.
-    	
-		//6. use a while loop to repeat step 5 until a robot has reached the top of the screen.
+	// 4. make each robot start at the bottom of the screen, side by side, facing up
 
-		//7. declare that robot the winner and throw it a party!
-    	
-		//8. try different races with different amounts of robots.
-    	
-	    //9. make the robots race around a circular track.
+	// 5. use another for loop to iterate through the array and make each robot move
+	// a random amount less than 50.
 
-	
-}
+	// 6. use a while loop to repeat step 5 until a robot has reached the top of the
+	// screen.
+
+	// 7. declare that robot the winner and throw it a party!
+
+	// 8. try different races with different amounts of robots.
+
+	// 9. make the robots race around a circular track.
+
+
