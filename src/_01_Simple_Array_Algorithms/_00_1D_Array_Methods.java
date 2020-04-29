442bc91cc33ab57212c29739b9c7c5f0c2605cc6
AaronD07 <38016960+AaronD07@users.noreply.github.com>
2020-04-29 15:30:08 -0700

    :smile:

--- src/_01_Simple_Array_Algorithms/_00_1D_Array_Methods.java
+++ src/_01_Simple_Array_Algorithms/_00_1D_Array_Methods.java
@@ -4,10 +4,11 @@ public class _00_1D_Array_Methods {
 	//1. Complete the method so that it returns the sum of all
 	//   of the integers in the array being passed in
 	public static int sumIntArray(int[] values) {
-
+		for(int o=0; o<values.length; o++) {
+int[]
 		return 0;
 	}
-	
+	}
 	//2. Complete the method so that it returns the average of all
 	//   of the integers in the array being passed in
 	public static double averageIntArray(int[] values) {
